1a33570378baee0d5ff151b376dd5cb4d1d70aca
Duc Nguyen <minhducnguyen189@gmail.com>
2023-10-13 15:28:43 +0700

    update spring security code grant type application

--- spring-security-code-grant-type/src/main/java/com/springboot/project/SpringSecurityCodeGrantTypeApplication.java
+++ spring-security-code-grant-type/src/main/java/com/springboot/project/SpringSecurityCodeGrantTypeApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class StoreSessionsDatabaseApplication {
+public class SpringSecurityCodeGrantTypeApplication {
     public static void main(String[] args) {
-        SpringApplication.run(StoreSessionsDatabaseApplication.class, args);
+        SpringApplication.run(SpringSecurityCodeGrantTypeApplication.class, args);
     }
 }
--- spring-security-code-grant-type/src/main/java/com/springboot/project/config/SecurityConfig.java
+++ spring-security-code-grant-type/src/main/java/com/springboot/project/config/SecurityConfig.java
@@ -1,47 +1,28 @@
 package com.springboot.project.config;
 
-import com.springboot.project.converter.KeycloakRoleConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.session.HttpSessionEventPublisher;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
-        jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(new KeycloakRoleConverter());
-
+    public SecurityFilterChain configure(HttpSecurity http) throws Exception {
         http.csrf(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests(requests -> requests
-                .anyRequest().authenticated())
-                .oauth2ResourceServer(oauth2 -> oauth2.jwt(jwtConfigurer -> jwtConfigurer.jwtAuthenticationConverter(jwtAuthenticationConverter)));
-
-        http.sessionManagement((sessionManagement) -> {
-                    sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                            .sessionConcurrency((sessionConcurrency -> sessionConcurrency.maximumSessions(1)));
-                });
+                .authorizeHttpRequests(auth ->
+                    auth.requestMatchers("/login/**")
+                            .permitAll()
+                            .anyRequest()
+                            .authenticated())
+                .oauth2Login(withDefaults());
         return http.build();
-    }
-
-    @Bean
-    public HttpSessionEventPublisher httpSessionEventPublisher() {
-        return new HttpSessionEventPublisher();
-    }
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
     }
 
 }
