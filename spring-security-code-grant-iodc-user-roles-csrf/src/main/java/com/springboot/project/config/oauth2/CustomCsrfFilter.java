822430078ad4b2d203bd95d277e2178aa78f5db4
ducnguyen <minhducnguyen189@gmail.com>
2023-12-25 21:15:36 +0700

    add example for spring security 6 with csrf

---
+++ spring-security-code-grant-iodc-user-roles-csrf/src/main/java/com/springboot/project/config/oauth2/CustomCsrfFilter.java
@@ -0,0 +1,32 @@
+package com.springboot.project.config.oauth2;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.web.csrf.CsrfToken;
+import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.web.util.WebUtils;
+
+import java.io.IOException;
+
+public class CustomCsrfFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        CsrfToken csrf = (CsrfToken) request.getAttribute("_csrf");
+
+        if (csrf != null) {
+            Cookie cookie = WebUtils.getCookie(request, "XSRF-TOKEN");
+            String token = csrf.getToken();
+            if (cookie == null || (token != null && !token.equals(cookie.getValue()))) {
+                cookie = new Cookie("XSRF-TOKEN", token);
+                cookie.setPath("/");
+                response.addCookie(cookie);
+            }
+        }
+        filterChain.doFilter(request, response);
+    }
+
+}
